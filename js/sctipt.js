da6f5f98ab3c1fe14c683e34ebbd8c5fb6598c25
ruslan <Rusakdemon000>
2021-04-12 20:22:09 -1200

    hw1

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,9 +1,28 @@
-const category = 'toys';
-console.log(`https://work.com${category}/5`);
+'use strict';
 
 
-const user = "Ivan";
+// const category = 'toys';
+// console.log(`https://work.com${category}/5`);
 
-alert('привет, {user}');
 
-console.log(us);
+
+const numberOfFilms = 'Сколько фильмов вы уже посмотрели?';
+
+const personalMovieDB = {
+    count : {},
+    movies : {},
+    actors : [],
+    genres : [],
+    privat : false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?'),
+      b = prompt( 'На сколько оцените его?'),
+      c = prompt( 'Один из последних просмотренных фильмов?'),
+      d = prompt( 'На сколько оцените его?')
+
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
