b1af2038488c848e1567243b04c147e39783d34b
ruslan <Rusakdemon000>
2021-04-12 23:34:15 -1200

    2

--- js/sctipt.js
+++ js/sctipt.js
@@ -6,33 +6,44 @@
 
 
 
-const numberOfFilms = 'Сколько фильмов вы уже посмотрели?';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
 const personalMovieDB = {
-    count : {},
+    count : numberOfFilms,
     movies : {},
     actors : [],
     genres : [],
     privat : false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-      b = prompt( 'На сколько оцените его?'),
-      c = prompt( 'Один из последних просмотренных фильмов?'),
-      d = prompt( 'На сколько оцените его?')
+// const a = prompt('Один из последних просмотренных фильмов?'),
+//       b = prompt( 'На сколько оцените его?'),
+//       c = prompt( 'Один из последних просмотренных фильмов?'),
+//       d = prompt( 'На сколько оцените его?')
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
-
-let num = 100;
-
-
-for(let i = 0; i < 20; i++){
-    num++;
-    console.log(num);
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?'),
+          b = prompt( 'На сколько оцените его?');
+    if(a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+        
+    }       
+    else{
+        i--;
+        console.log('error');
+    }
+    console.log(personalMovieDB);
 }
 
-console.log(num);
+
+
+if(personalMovieDB.count > 5 ) {
+    console.log("not bad");
+}
+else{
+    console.log("baddd");
+}
+
+// переписать цикл еще двумя способами
