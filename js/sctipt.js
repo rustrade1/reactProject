9e613d749c3ca8554668d70b6cd8c8c48d0de832
ruslan <Rusakdemon000>
2021-04-13 21:29:54 -1200

    hw3

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,6 +1,5 @@
+
 'use strict';
-
-
 // const category = 'toys';
 // console.log(`https://work.com${category}/5`);
 
@@ -16,34 +15,41 @@ const personalMovieDB = {
     privat : false
 };
 
-// const a = prompt('Один из последних просмотренных фильмов?'),
-//       b = prompt( 'На сколько оцените его?'),
-//       c = prompt( 'Один из последних просмотренных фильмов?'),
-//       d = prompt( 'На сколько оцените его?')
 
 
-
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?'),
-          b = prompt( 'На сколько оцените его?');
-    if(a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
-        personalMovieDB.movies[a] = b;
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?'),
+//           b = prompt( 'На сколько оцените его?');
+//     if(a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
+//         personalMovieDB.movies[a] = b;
         
-    }       
-    else{
-        i--;
-        console.log('error');
-    }
-    console.log(personalMovieDB);
+//     }       
+//     else{
+//         i--;
+//         console.log('error');
+//     }
+//     console.log(personalMovieDB);
+// }
+
+// if(personalMovieDB.count > 5 ) {
+//     console.log("not bad");
+// }
+// else{
+//     console.log("baddd");
+// }
+
+let num = 0;
+while (num < 2) {
+    let a = prompt('Один из последних просмотренных фильмов?'),
+        b = prompt( 'На сколько оцените его?');
+        if (a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
+            personalMovieDB.movies[a] = b;
+            num++;
+        }
+        else{
+            num--;
+        }
+  
 }
+console.log(personalMovieDB);
 
-
-
-if(personalMovieDB.count > 5 ) {
-    console.log("not bad");
-}
-else{
-    console.log("baddd");
-}
-
-// переписать цикл еще двумя способами2
