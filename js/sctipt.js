86c160ebec1c6fa5d21385438387058f4d030b4f
ruslan <Rusakdemon000>
2021-04-14 20:26:12 -1200

    hw4Functions

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,55 +1,78 @@
 
 'use strict';
-// const category = 'toys';
-// console.log(`https://work.com${category}/5`);
-
-
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+let numberOfFilms;
 
 const personalMovieDB = {
     count : numberOfFilms,
     movies : {},
     actors : [],
     genres : [],
-    privat : false
+    privat : true
 };
 
 
+// function start() {
+//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
-// for (let i = 0; i < 2; i++) {
-//     const a = prompt('Один из последних просмотренных фильмов?'),
-//           b = prompt( 'На сколько оцените его?');
-//     if(a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
-//         personalMovieDB.movies[a] = b;
-        
-//     }       
-//     else{
-//         i--;
-//         console.log('error');
+//     while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 //     }
+// }
+
+// start();
+
+
+//  function rememberMyFilms() {    
+//     for (let i = 0; i < 2; i++) {
+//         const a = prompt('Один из последних просмотренных фильмов?'),
+//               b = prompt( 'На сколько оцените его?');
+//         if(a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
+//             personalMovieDB.movies[a] = b;
+            
+//         }       
+//         else{
+//         i--;
+//             console.log('error');
+//         }
+//     }
+//  }
+
+// rememberMyFilms();
+
+
+// // function detectPersonalLevel() {        
+// //     if(personalMovieDB.count > 5 ) {
+// //         console.log("not bad");
+// //     }
+// //     else{
+// //         console.log("baddd");
+// //     }
+// // }
+
+// // detectPersonalLevel();
+
+// // 2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+// // false - выводит в консоль главный объект программы
+
+// function showMyDB() {
+//     if(personalMovieDB.privat !== false){
 //     console.log(personalMovieDB);
+//     }
 // }
+// showMyDB();
 
-// if(personalMovieDB.count > 5 ) {
-//     console.log("not bad");
-// }
-// else{
-//     console.log("baddd");
-// }
 
-let num = 0;
-while (num < 2) {
-    let a = prompt('Один из последних просмотренных фильмов?'),
-        b = prompt( 'На сколько оцените его?');
-        if (a != "" && b != "" && a != null && b != null && a.length < 50 && b.length < 50) {
-            personalMovieDB.movies[a] = b;
-            num++;
-        }
-        else{
-            num--;
-        }
-  
+// 3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+// "Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+// genres
+
+
+function writeYourGenres() {
+    for(let i=1; i < 4; i++) {
+       let film = prompt(`Ваш любимый жанр под номером ${i}`);
+       personalMovieDB.genres[i-1] = film;
+    }
 }
-console.log(personalMovieDB);
 
+writeYourGenres();
+console.log(personalMovieDB);
